c06e0a5b26e27ca8d7eae624ba50686af4f2ecc3
RahulDiwakar <54063647+RahulDiwakar@users.noreply.github.com>
2021-01-02 17:26:50 +0530

    Update script.js

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -2,15 +2,12 @@
 
 var names = ["Rahul", "Pranav", "Shashank", "Mahesh", "Aditya", "Gaurav", "Raghu", "Lokesh"];
 
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'r') {
-    window.byeSpeaker.speak(names[i]);
-  }
-  else {
-    window.helloSpeaker.speak(names[i]);
+for (var i in names) {  
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
   }
 }
-
-})();
+})(window);
