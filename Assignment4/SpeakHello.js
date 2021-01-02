9e0ae55f53613f0e56b231a83d95314b544c66db
RahulDiwakar <54063647+RahulDiwakar@users.noreply.github.com>
2021-01-02 17:28:39 +0530

    Update SpeakHello.js

--- Assignment4/SpeakHello.js
+++ Assignment4/SpeakHello.js
@@ -1,12 +1,9 @@
-(function (window) {
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
 
-
-var helloSpeaker = {};
-
-helloSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-
-window.helloSpeaker = helloSpeaker;
+	window.helloSpeaker = helloSpeaker;
 
 })(window);
